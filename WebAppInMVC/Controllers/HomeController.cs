2fa8f05b7633cd5e0c641ca28edd2047f7010d04
tomclock <wangjij@gmail.com>
2016-07-17 00:17:38 +0900

    added the ett

--- WebAppInMVC/Controllers/HomeController.cs
+++ WebAppInMVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
@@ -12,36 +13,14 @@ namespace WebAppInMVC.Controllers
     public class HomeController : Controller
     {
         public IEnumerable<Product> Products { get; private set; }
+        private IotTestEntities DbEntities = new IotTestEntities();
 
-        private ProductListViewModel productListVM = new ProductListViewModel();
-
-        //public ViewResult List()
-        //{
-        //    var productList = new List<SelectListItem>();
-
-        //    Products = new List<Product>
-        //    { new Product {Name = "Tom", Price = 100, ProductId = "1" },
-        //      new Product { Name = "Jack", Price = 101, ProductId = "2" },
-        //      new Product { Name = "Tim", Price = 101, ProductId = "3" },
-        //      new Product { Name = "Hack", Price = 101, ProductId = "4" }};
-
-        //    foreach (Models.Product p in Products)
-        //    {
-        //        productList.Add(new SelectListItem
-        //        {
-        //            Value = p.ProductId.ToString(),
-        //            Text = "Product: " + p.Name + " " + p.Price.ToString(),
-        //            // To set the selected item use the following code 
-        //            // Note: you should not set every item to selected
-        //            Selected = true
-        //        });
-        //    }
-        //    productListVM.Products = productList;
-
-        //    return View(productListVM);
-        //}
-
-        public ActionResult Index()
+        #region MyRegion + Index()
+        /// <summary>
+        /// To get the data by self creation
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index2()
         {
 
             //initialize the Product contents. 
@@ -65,10 +44,25 @@ namespace WebAppInMVC.Controllers
                     Selected = true
                 });
             }
+            ProductListViewModel productListVM = new ProductListViewModel();
             productListVM.Products = productList;
 
             return View(productListVM);
         }
+        #endregion
+
+        #region MyRegion + Index()
+        public ActionResult Index()
+        {
+            //show the tables in the entities
+            var query = from e in DbEntities.tb_terminal
+                        select new SelectListItem { Value = e.device_id, Text = e.device_id };
+
+            ViewBag.SelectLists = query.ToList();
+
+            return View();
+        } 
+        #endregion
 
         public ActionResult About()
         {
